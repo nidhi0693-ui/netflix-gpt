d102ff07ad9a2ac1246d9cdaf228ebcdb6311961
Nidhi Kumari <nidhi.kumari@mtxb2b.com>
2023-11-10 09:15:57 +0530

    firebase initial setup

--- README.md
+++ README.md
@@ -40,6 +40,10 @@ Now you created a react app with tailwind css successfully. Now run the below co
 - SignInForm / SignUpForm
 - Form Validation
 - useRef hook
+- Firebase Setup
+- Deploying app to production
+- Create Sign Up User Account in Firebase
+- 
 
 - Browse Page
 
---
+++ src/utils/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB7mDzD2Otu03ittQc6CC2g8r2wX3f1hDg",
+  authDomain: "netflixgpt-f4425.firebaseapp.com",
+  projectId: "netflixgpt-f4425",
+  storageBucket: "netflixgpt-f4425.appspot.com",
+  messagingSenderId: "985359477779",
+  appId: "1:985359477779:web:9291faeb5ebaec880323e6",
+  measurementId: "G-QREL7E5GTE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
